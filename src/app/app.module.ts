2ebd9608bcfab3fecf69a0a9365d96f27ba1ba1b
wesleyselmer <wesleyselmer@gmail.com>
2023-11-06 14:06:38 -0300

    Criando page Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CheckboxModule } from 'primeng/checkbox';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  declarations: [AppComponent, LoginComponent, HomeComponent, NavbarComponent, FooterComponent],
+  imports: [BrowserModule, 
+    BrowserAnimationsModule, 
+    AppRoutingModule,
+    CheckboxModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
